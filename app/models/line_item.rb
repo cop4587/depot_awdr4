7ece3aa07776a00bf1da329af207d01075177d23
mabo <mabo.cn@qq.com>
2014-05-02 20:34:59 +0800

    done with task d - can add item to cart

---
+++ app/models/line_item.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  cart_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_line_items_on_cart_id     (cart_id)
+#  index_line_items_on_product_id  (product_id)
+#
+
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+end
